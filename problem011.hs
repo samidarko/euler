0e58770e83b1374c072f4c57287cb497bd0fece6
samidarko <samidarko@gmail.com>
2017-08-29 20:43:58 +0800

    problem 11: simplification + added some hints for diagonal

--- problem011.hs
+++ problem011.hs
@@ -1,22 +1,19 @@
 import Data.List (transpose)
-import Data.List.Split (splitOn)
+import Data.List.Split (splitOn, chunksOf)
 
 main :: IO ()
 main = do
   content <- readFile "problem011.txt"
   let l = [ read x :: Int | x <- foldl (++) [] $ map (splitOn " ") (lines content)]
-  let rows = getRows 20 l
+  let rows = getRows l
   let cols = getColums rows
   let maxRows = maximum $ map (maxAdjacent 0) rows
   let maxCols = maximum $ map (maxAdjacent 0) cols
   putStrLn ("maxCols: " ++ show maxCols)
   putStrLn ("maxRows: " ++ show maxRows)
 
-getRows :: Int -> [Int] -> [[Int]]
-getRows x xs = fn xs
-  where fn ys = let h = take x ys
-                    t = drop x ys
-                in if (not $ null t) then h : (fn t) else [h]
+-- NW - SE => [ l !! (20 * x + x) | x <- [0..19]]
+-- NE - SW => [ l !! (19 * x) | x <- [1..20]]
  
 getColums :: [[Int]] -> [[Int]]
 getColums xs = transpose xs
@@ -27,3 +24,11 @@ maxAdjacent m xs = let adjacents = 4
                        p = product s
                    in if (length s < adjacents) then m else maxAdjacent (max m p) (tail xs)
 
+getRows :: [Int] -> [[Int]]
+getRows xs = chunksOf 20 xs
+
+-- getRows :: Int -> [Int] -> [[Int]]
+-- getRows x xs = fn xs
+--   where fn ys = let h = take x ys
+--                     t = drop x ys
+--                 in if (not $ null t) then h : (fn t) else [h]
