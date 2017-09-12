20e32f4d3f707b918d439974d4cf4e35347c1df7
samidarko <samidarko@gmail.com>
2017-09-12 20:48:14 +0800

    problem 40 (in progress)

--- problem040.hs
+++ problem040.hs
@@ -1,5 +1,27 @@
 import Data.Char (digitToInt)
 
+nbOfDigits = [2..7]
+indexes = [10, 100, 1000, 10000, 100000, 1000000]
+digitsBySeq = [ nbOfDigits !! i * indexes !! i * 9 * 10^i
+  | i <- [0..5] ] 
+
+-- champernowne :: Int
+-- champernowne = 1
+--   where a = [180, 2700, 36000, 450000, 5400000, 63000000]
+
+extract x = if (x < 1000) then
+                          digitToInt $ foldl (++) [] [show x | x <- [0..100]] !! x
+                          else 1
+
+
+
+-- 1000000-3889
+-- 10 * 1 + 90 * 2 + 900 * 3
+-- 10000 - 2890
+-- 10 * 1 + 90 * 2 + 900 * 3 + 9000 * 4
+-- [1000..9999] !! 1777 
+-- 1777*4
+-- 7110 - 1777*4
 -- takeWhile ((<3) . length)  [ show x | x <- [0..]]
 --
 -- l = foldl (++) [] [show x | x <- [1..100]]
@@ -21,3 +43,7 @@ import Data.Char (digitToInt)
 -- 450000
 -- *Main Prelude Set Data.Char Data.Char Char> (999999 - 99999) * 5
 -- 4500000
+-- 1 * 1 * 5 * 3 * 7
+-- 10 * 1 + 90 * 2 + 900 * 3 + 9000 * 4
+-- 38890
+
