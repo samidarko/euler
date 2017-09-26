e9e13993cb5fd892c0951e47c1baac625891dff8
samidarko <samidarko@gmail.com>
2017-09-26 19:56:38 +0800

    problem 92 (solved)

--- problem092.hs
+++ problem092.hs
@@ -2,27 +2,31 @@ import Helpers (stringToIntList)
 import Control.Monad.State
 import qualified Data.Map as M
 
-next x = (sum . map (^2) . stringToIntList . show) x
+next x = (sum . map (^2) . stringToIntList . show) x    -- TODO probably the bottle neck
 
-iter y
+iter :: M.Map Integer Integer -> Integer -> Integer
+iter m y
   | y == 89 = 89
   | y == 1 = 1
-  | otherwise = iter $ next y
+  | otherwise = case (M.lookup y m) of
+      Just(x) -> x
+      _ -> iter m (next y)
 
-chain x = case (iter x) of 
-            89 -> Right x
-            1 -> Left x
+type Counter = Integer
+type SquareDigitState = (M.Map Integer Integer, Counter)
 
-rightCounter e = case e of Right _ -> 1; _ -> 0
-
-type Counter = Int
-type SquareDigitState = (M.Map Int Int, Counter)
-
-squareDigitChains :: [Int] -> State SquareDigitState Counter
+squareDigitChains :: [Integer] -> State SquareDigitState Counter
 squareDigitChains [] = do
     (_, counter) <- get
     return counter
 squareDigitChains (x:xs) = do
+    (mem, counter) <- get
+    case (iter mem x) of
+        89 -> put (M.insert x 89 mem, counter + 1)
+        1 -> put (M.insert x 1 mem, counter)
+    squareDigitChains xs
 
--- squareDigitChains = foldl (\acc v -> acc + rightCounter v) 0 (map chain [1..100000])
+-- *Main> evalState (squareDigitChains [1..10000000]) (M.fromList [], 0)
+-- 8581146
+-- (150.83 secs, 107,084,852,688 bytes)
 
