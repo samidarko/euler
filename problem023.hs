ddd0f639129e5cef4bd6559fd8657d10e8e06c42
samidarko <samidarko@gmail.com>
2017-11-19 16:35:27 +0800

    problem 23 (solved)

--- problem023.hs
+++ problem023.hs
@@ -1,5 +1,16 @@
-import Helpers (factors)
+import Data.List (group)
+import Data.Numbers.Primes (primeFactors)
 
-abondantNumbers = [ x | x <- [1..28123], let y = sum $ factors x, x < y]
+sumPower :: [Int] -> Int
+sumPower xs = let b = head xs
+               in (1 - b ^ (length xs + 1)) `div` (1 - b)
 
+properDivisorSum x = (product $ map sumPower $ group $ primeFactors x) - x
+
+isAbondunt x = properDivisorSum x > x
+
+abonduntNumbers = sum [ x | x <- [1..limit], x `notElem` absl ]
+  where abl = filter isAbondunt [2..limit]
+        absl = [ x | i <- [0..length abl - 1], j <- [i..length abl - 1], let x = (abl !! i) + (abl !! j), x <= limit ]
+        limit = 28123
 
